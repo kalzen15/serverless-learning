271533d81b1a7b694dbe9bfd78d2b5b163bc829e
Calvin D'Souza <calvin091518dsouza@gmail.com>
2021-05-11 16:59:39 +0530

    styled-components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import './App.css';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
-import Game from './pages/Game'
-import GameOver from './pages/GameOver'
-import HighScores from './pages/HighScores'
-import Home from './pages/Home'
-import NavBar from './components/NavBar';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Game from "./pages/Game";
+import GameOver from "./pages/GameOver";
+import HighScores from "./pages/HighScores";
+import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+import { Container } from "./styled/Container";
+import { Main } from "./styled/Main";
+import Global from "./styled/Global";
 
 function App() {
   return (
     <Router>
-      <NavBar/>
-      <Switch>
-         <Route path="/game" component={Game}/>
-         <Route path="/highScores" component={HighScores}/>
-         <Route path="/gameOver" component={GameOver}/>
-         <Route path="/" component={Home}/>
-      </Switch>
+      <Global />
+      <Main>
+        <Container>
+          <NavBar />
+          <Switch>
+            <Route path="/game" component={Game} />
+            <Route path="/highScores" component={HighScores} />
+            <Route path="/gameOver" component={GameOver} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Container>
+      </Main>
     </Router>
   );
 }
